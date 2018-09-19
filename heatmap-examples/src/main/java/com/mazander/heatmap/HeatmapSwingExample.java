a184f918e90b6e3968b94acd58a36f4be8fcc2cc
mazander <matti.kannala@gmail.com>
2018-09-19 23:30:36 +0300

    Add map to Swing example

--- heatmap-examples/src/main/java/com/mazander/heatmap/HeatmapSwingExample.java
+++ heatmap-examples/src/main/java/com/mazander/heatmap/HeatmapSwingExample.java
@@ -2,10 +2,13 @@ package com.mazander.heatmap;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,24 +21,36 @@ public class HeatmapSwingExample {
 		for (int i = 0; i < 100; i++) {
 			double x = Math.random() * 1000.0;
 			double y = Math.random() * 1000.0;
-			PointHeat pointHeat = new PointHeat(x, y, 0.5, 100.0);
+			PointHeat pointHeat = new PointHeat(x, y, 50.0, 100.0);
 			pointHeat.setAttenuation(Attenuations.QUADRATIC);
 			heatSources.add(pointHeat);
 		}
 		BinaryTreeHeatmap heatmap = new BinaryTreeHeatmap(heatSources);
 
 		HeatmapRenderer rendeder = new HeatmapRenderer(1000, 1000);
+		rendeder.setMaxHeat(100.0);
 		rendeder.setBounds(new Bounds(0.0, 0.0, 1000.0, 1000.0));
 
 		AlphaColorScheme alphaColorScheme = new AlphaColorScheme(ColorSchemes.JET, 0.0, 1.0, Attenuations.QUADRATIC);
-		final BufferedImage image = rendeder.render(heatmap, alphaColorScheme);
+		BufferedImage mapImage;
+		try {
+			mapImage = ImageIO.read(HeatmapSwingExample.class.getResourceAsStream("/com/mazander/heatmap/helsinki.png"));
+		} catch (IOException e) {
+			mapImage = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
+		}
+		
+		Graphics2D graphics = mapImage.createGraphics();
+		
+		BufferedImage image = rendeder.render(heatmap, alphaColorScheme);
+		graphics.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+		graphics.dispose();
 		
 		JFrame frame = new JFrame("Heatmap");
 		frame.setSize(image.getWidth(), image.getHeight());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.getContentPane().setLayout(new BorderLayout());
-		frame.getContentPane().add(new JLabel(new ImageIcon(image)), BorderLayout.CENTER);
+		frame.getContentPane().add(new JLabel(new ImageIcon(mapImage)), BorderLayout.CENTER);
 		SwingUtilities.invokeLater(() -> frame.setVisible(true));
 	}
 
